2267bdf3876462928a443048752398205740cdb5
whywt <tao.wang@sharpg.com>
2020-11-17 11:35:56 +0800

    my first Go

---
+++ HelloWorld.go
@@ -0,0 +1,23 @@
+package main
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	
+	var a int32 = 1
+	var b = a
+	fmt.Println(a,b)
+
+	b = 2
+	fmt.Println(a,b)
+
+	Swap(a,b)
+	fmt.Println(a,b)
+
+	a, b = Swap(a,b)
+	fmt.Println(a,b)
+}
+
+func Swap(a,b int32) (int32, int32) {
+	return b, a
+}
